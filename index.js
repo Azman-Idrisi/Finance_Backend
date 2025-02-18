8b4d54441dd49c5a9a60265bec0decb1b2801853
Azman <idrisiazman@gmail.com>
2025-02-18 22:46:41 +0530

    base url updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Adjust this to match your frontend URL
+    origin: "https://finance-seven-mu.vercel.app", // Adjust this to match your frontend URL
     methods: ["GET", "POST"]
   }
 });
